9a195cca688a5300a1111f0576f093978c7af938
Abhishek <57477090+abhishekknvk@users.noreply.github.com>
2024-05-30 14:11:57 +0530

    all

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const list = document.querySelector('#movie-list ul');
+    const forms = document.forms;
+
+    // delete movie
+    list.addEventListener('click', (e)=>{
+        
+    })
+})
