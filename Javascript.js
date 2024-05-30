0c99652fa7d0edd23d7dcca8c3d373e98ebf11fe
Abhishek <57477090+abhishekknvk@users.noreply.github.com>
2024-05-30 14:45:21 +0530

    update

--- Javascript.js
+++ Javascript.js
@@ -4,6 +4,39 @@ document.addEventListener('DOMContentLoaded',function(){
 
     // delete movie
     list.addEventListener('click', (e)=>{
-        
-    })
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+    }      
+    });
+
+    // add movie
+    const addForm = orms['add-movie'];
+    addForm.addEventListener('submit', function(e){
+        e.parentDefault();
+
+
+        //Creating Elements
+        const value =addForm.querySelector("input[type ='text']").value;
+        const li= document.createElement(li);
+        const movieName = document.createElement('span');
+        const deleteBtn = document.createElement('span');
+
+        // add txt content
+    movieName.textContent = value;
+    deleteBtn.textContent  ='delete ';
+
+
+    //add classes
+
+    movieName.classList.add('name');
+    deletebtn.classList.add('delete');
+
+    //append to DOM
+
+    li.appendChild(movieName);
+    li.appendChild(deleteBtn);
+    list.append(li);
+
+    });
 })
