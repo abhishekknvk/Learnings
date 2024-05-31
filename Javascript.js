ab23fc8de2a835343efa81c74de919de0b493868
Abhishek <57477090+abhishekknvk@users.noreply.github.com>
2024-05-31 12:58:36 +0530

    update

--- Javascript.js
+++ Javascript.js
@@ -18,9 +18,27 @@ document.addEventListener('DOMContentLoaded',function(){
 
         //Creating Elements
         const value =addForm.querySelector('input[type ="text"]').value;
+
+         // Input Validation
+    if(value === '' || value == null) {
+        alert('Please enter a movie name');
+        return;
+    }
+
         const li= document.createElement('li');
         const movieName = document.createElement('span');
         const deleteBtn = document.createElement('span');
+
+         // Create stars
+    const starsContainer = document.createElement('div');
+    for(let i = 1; i <= 5; i++) {
+        const star = document.createElement('span');
+        star.textContent = '★';
+        star.dataset.value = i;
+        star.className = 'star';
+        starsContainer.appendChild(star);
+    }
+
 
         // add txt content
     movieName.textContent = value;
